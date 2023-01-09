2679a332e329fb8542b8430dcdc4ec9a8cabaece
rpaka <ritsuki.kokubo@gmail.com>
2023-01-09 17:39:12 +0900

    チェックマークが付いてないもので判別するように変更

--- src/contentScript.js
+++ src/contentScript.js
@@ -2,12 +2,12 @@
 
 console.log(`[ドネートチェック自動化] 初期化処理...`);
 
-var lastCount = 0;
 var run = true;
+const TM_POLL = 1000; // ms
 
 function runFn() {
   if (!run) {
-    setTimeout(runFn, 1000);
+    setTimeout(runFn, TM_POLL);
     return;
   }
 
@@ -19,37 +19,32 @@ function runFn() {
     var contentFrame = document.getElementById("missionControl");
     var contentDocument = contentFrame.contentWindow.document;
   } catch (e) {
-    setTimeout(runFn, 1000);
+    setTimeout(runFn, TM_POLL);
     return;
   }
 
   var donateItems = contentDocument.querySelectorAll(".event-donation");
 
-  var diffCount = donateItems.length - lastCount;
-  lastCount = donateItems.length;
-  var execCount = 0;
-
-  if (0 < diffCount) {
-    console.log(`[ドネートチェック自動化] 新規検出${diffCount}件`);
-  }
-
+  var rowNo = 1;
   for (var donateItem of donateItems) {
     try {
-      if (diffCount < ++execCount) break;
-
+      var row = donateItem.querySelector(".table__info > div");
       var opBtn = donateItem.querySelector(".table__button > button");
       var opBtnMark = opBtn.querySelector("i");
       var opBtnIsChecked = opBtnMark.className.split(' ').includes('fa-check');
 
       if (opBtnIsChecked) continue;
       opBtn.click();
-      console.log("[ドネートチェック自動化] クリックしました");
+      row.click();
+      console.log(`[ドネートチェック自動化] ${rowNo}行目をクリックしました`);
+
+      rowNo++;
     } catch (e) {
       console.log(e.message);
     }
   }
 
-  setTimeout(runFn, 1000);
+  setTimeout(runFn, TM_POLL);
 }
 
 window.addEventListener("load", function () {
@@ -58,7 +53,7 @@ window.addEventListener("load", function () {
       result.enable = true;
     }
     run = result.enable;
-    setTimeout(runFn, 1000);
+    setTimeout(runFn, TM_POLL);
   });
 })
 
@@ -70,3 +65,5 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   sendResponse({});
   return true;
 });
+
+console.log(`[ドネートチェック自動化] 初期化処理完了`);
